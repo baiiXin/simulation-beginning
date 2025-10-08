Correct sign error of contact force

hessian_validator/hessian_point_validator.py
@@ -166,10 +166,10 @@ class HessianPointValidator(HessianValidator):
 
 
         # 使用梯度函数的有限差分计算Hessian
-        force = numerical_grad(f_force, x_a, h=self.epsilon)
+        force = -numerical_grad(f_force, x_a, h=self.epsilon)
 
         # 使用梯度函数的有限差分计算Hessian
-        hessian = numerical_jacobian(f_hessian, x_a, h=self.epsilon)
+        hessian = -numerical_jacobian(f_hessian, x_a, h=self.epsilon)
         
         return force, hessian
     
@@ -186,7 +186,7 @@ class HessianPointValidator(HessianValidator):
         grad = torch.autograd.grad(energy, x_a, create_graph=True)[0]
         
         # print("自动微分计算Force:", grad)
-        force = grad
+        force = -grad
 
         # 计算Hessian
         hessian = torch.zeros((n, n), device=self.device)

hessian_validator/run_two_point.py
@@ -21,7 +21,7 @@ class ContactEnergy:
 
     def analytical_force(self, x_a, x_b):
         """解析力：对于弹簧能量就是力"""
-        return -self.stiffness * (self.collision_radius - torch.dot(self.hat_normal, x_a - x_b)) * self.hat_normal
+        return self.stiffness * (self.collision_radius - torch.dot(self.hat_normal, x_a - x_b)) * self.hat_normal
     
     def analytical_hessian(self, x_a, x_b):
         """解析Hessian：对于弹簧能量就是刚度矩阵"""
@@ -63,7 +63,7 @@ class LineEnergy:
 test_point=torch.tensor([[1.0, 1.0, 1.0], [0.0, 0.0, 0.0]], device='cpu')
 
 # 使用
-energy = LineEnergy()
+energy = ContactEnergy(point=test_point)
 print('\nceshi:', energy(test_point[0], test_point[1]),'\n')
 
 # 点验证